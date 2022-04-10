0192aca124f0cccefe9be63725f27b4093884c8a
Robert Pofuk <rpofuk@gmail.com>
2022-04-10 15:11:28 +0200

    Update builsh

--- build.sh
+++ build.sh
@@ -7,5 +7,7 @@ echo "Building ${BUILD_ID}"
 docker build --progress=plain \
 	     --no-cache \
 	     --build-arg BUILD_ID="${BUILD_ID}" \
+	     --build-arg BuildId="${BUILD_ID}" \
+	     --build-arg AWS_REGION="${AWS_DEFAULT_REGION}" \
 	     -t alpha-gerrit-svc:b${BUILD_ID} \
 	     -f Dockerfile .
