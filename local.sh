e4ee38992203ae9e0db28008e651915747ca2ba2
Robert Pofuk <rpofuk@gmail.com>
2022-04-10 15:09:59 +0200

    Update local script

--- local.sh
+++ local.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-export BUILD_ID="6"
+export TARGET_ACCOUNT_ID="$(aws sts get-caller-identity | jq -r '.Account')"
+export DOCKER_BUILDKIT=0
+export DOCKER_BUILDKIT=1
 
+export TARGET=ubuntu@${1}
+if [[ "${1:-}" != "" ]]; then
+  export DOCKER_HOST=ssh://${TARGET}
+fi
+export BUILD_ID="7"
+
+export AWS_DEFAULT_REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
 export TARGET_ACCOUNT_ID="$(aws sts get-caller-identity | jq -r '.Account')"
 
 ./build.sh && ./run.sh
