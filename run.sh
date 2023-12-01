0ef24909da88818fe19588bd92e336bc586a9f59
Robert Pofuk <rpofuk@gmail.com>
2023-12-01 10:02:32 +0100

    Make run smarter

--- run.sh
+++ run.sh
@@ -4,6 +4,22 @@ set -e
 
 echo "Going to deploy me some stuff"
 
+export TARGET_ACCOUNT_ID="$(aws sts get-caller-identity | jq -r '.Account')"
+export DOCKER_BUILDKIT=0
+export DOCKER_BUILDKIT=1
+
+export LATEST_IMAGE="$(aws ec2 describe-images \
+                          --owners self --no-paginate  \
+			  | jq -r '.Images[].Name' \
+			  | grep build-alpha-gerrit  \
+			  | sort | tail -1)"
+echo "Last image found: $LATEST_IMAGE"
+export BUILD_ID="${LATEST_IMAGE##*.b}"
+echo "Deploy build id: $BUILD_ID"
+
+export AWS_DEFAULT_REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+
+
 curl https://raw.githubusercontent.com/raiffeisenbankinternational/cbd-jenkins-pipeline/master/ext/deploy.sh > deploy.sh
 chmod +x deploy.sh
 
