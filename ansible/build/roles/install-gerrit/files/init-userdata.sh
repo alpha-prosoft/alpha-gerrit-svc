95cc358f08d91d7f965e285ae78b911ece880193
pofuk <pofuk@walter-group.com>
2026-01-04 09:23:14 +0100

    Cleanup for update

--- ansible/build/roles/install-gerrit/files/init-userdata.sh
+++ ansible/build/roles/install-gerrit/files/init-userdata.sh
@@ -1,6 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
 
-set -euo pipefail 
+set -euo pipefail
 
 echo "Starting gerrit startup"
 
@@ -37,14 +37,12 @@ su - gerrit -s /bin/sh \
 cp /opt/gerrit/plugins/* /var/lib/gerrit/plugins/
 chown gerrit:gerrit /var/lib/gerrit/plugins -R
 
-
-
 su - gerrit -s /bin/sh \
-     -c "git config -f /var/lib/gerrit/etc/gerrit.config \
+  -c "git config -f /var/lib/gerrit/etc/gerrit.config \
                    gerrit.canonicalWebUrl \"https://${ServiceAlias}.${PrivateHostedZoneName}\""
 
-su - gerrit  -s /bin/sh \
-     -c "git config -f /var/lib/gerrit/etc/gerrit.config \
+su - gerrit -s /bin/sh \
+  -c "git config -f /var/lib/gerrit/etc/gerrit.config \
                    sendemail.enable false"
 
 ls -la /var/lib/gerrit/etc/gerrit.config
@@ -54,56 +52,53 @@ systemctl start gerrit
 echo "Usefull for first login to setup propper admin"
 echo "We configured gerrit to trust A-User header as propper login"
 curl --fail -c cookie.txt http://127.0.0.1:8082/login \
-        -H "A-User: admin" \
-        -H "A-Email: admin@${PrivateHostedZoneName}" \
-        -H "A-Name: Administrator"
+  -H "A-User: admin" \
+  -H "A-Email: admin@${PrivateHostedZoneName}" \
+  -H "A-Name: Administrator"
 
 # CSRF Token is sent on next request
 curl --fail -c cookie.txt -b cookie.txt http://127.0.0.1:8082/ \
-        -H "A-User: admin" \
-        -H "A-Email: admin@${PrivateHostedZoneName}" \
-        -H "A-Name: Administrator"
+  -H "A-User: admin" \
+  -H "A-Email: admin@${PrivateHostedZoneName}" \
+  -H "A-Name: Administrator"
 
-auth_token=$(cat cookie.txt  | grep XSRF_TOKEN | awk '{printf $7}')
+auth_token=$(cat cookie.txt | grep XSRF_TOKEN | awk '{printf $7}')
 
-sudo cat /home/${Username}/.ssh/id_rsa.pub  | \
-         curl --fail  --data @- \
-         -b cookie.txt  \
-         -H "Content-Type: text/plain" \
-         -H "X-Gerrit-Auth: ${auth_token}" \
-         http://127.0.0.1:8082/accounts/self/sshkeys
-
+sudo cat /home/${Username}/.ssh/id_rsa.pub |
+  curl --fail --data @- \
+    -b cookie.txt \
+    -H "Content-Type: text/plain" \
+    -H "X-Gerrit-Auth: ${auth_token}" \
+    http://127.0.0.1:8082/accounts/self/sshkeys
 
 echo "Loading custom certificates"
 parameter_names=$(aws ssm get-parameters-by-path \
-                     --path "/${EnvironmentNameLower}/keys/public/" \
-                     --recursive \
-                     --query 'Parameters[*].[Name]' \
-                     --output text)
-                     
-for parameter_name in $parameter_names; do \
-   echo "Processing ${parameter_name}"; \
-   param_path="$(dirname ${parameter_name})"
-   service="$(basename ${param_path})"
-   key_file=$(mktemp)
-   echo "Key file $key_file for service ${service}"
-   aws ssm get-parameter \
-     --name ${parameter_name} \
-     --with-decryption \
-     --query 'Parameter.Value' \
-     --output text > $key_file
+  --path "/${EnvironmentNameLower}/keys/public/" \
+  --recursive \
+  --query 'Parameters[*].[Name]' \
+  --output text)
 
-   ssh -i /home/${Username}/.ssh/id_rsa admin@127.0.0.1 -p 29418 -oStrictHostKeyChecking=no \
-            gerrit create-account \
-              --group "'Service Users'"  \
-              --full-name "${service^}" \
-              --email "${service}@${PrivateHostedZoneName}" ${service} || echo "User exits"
+for parameter_name in $parameter_names; do
+  echo "Processing ${parameter_name}"
+  param_path="$(dirname ${parameter_name})"
+  service="$(basename ${param_path})"
+  key_file=$(mktemp)
+  echo "Key file $key_file for service ${service}"
+  aws ssm get-parameter \
+    --name ${parameter_name} \
+    --with-decryption \
+    --query 'Parameter.Value' \
+    --output text >$key_file
 
-   echo "Updating public key for ${service}"
-   cat $key_file |
-     ssh -i /home/${Username}/.ssh/id_rsa admin@127.0.0.1 -p 29418 \
-       gerrit set-account --add-ssh-key - ${service}
-   rm -f $key_file
+  ssh -i /home/${Username}/.ssh/id_rsa admin@127.0.0.1 -p 29418 -oStrictHostKeyChecking=no \
+    gerrit create-account \
+    --group "'Service Users'" \
+    --full-name "${service^}" \
+    --email "${service}@${PrivateHostedZoneName}" ${service} || echo "User exits"
+
+  echo "Updating public key for ${service}"
+  cat $key_file |
+    ssh -i /home/${Username}/.ssh/id_rsa admin@127.0.0.1 -p 29418 \
+      gerrit set-account --add-ssh-key - ${service}
+  rm -f $key_file
 done
-
-
